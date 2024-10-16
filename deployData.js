531d13777c1a8cd99cf9ea79fd9bbc096cfac0ce
GhilbranRafif <mulyaghilbran@gmail.com>
2024-10-16 16:58:53 +0700

    First Commit : Working CRUD

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Ghilbran Rafif Mulya"; // TODO: replace this
+
+const studentId = "FS11109397"; // TODO: replace this
+
+const NetlifyDeployUrl = "TODO: replace me"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
